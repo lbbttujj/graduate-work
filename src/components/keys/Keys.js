03627815710d45d11614cbc38b9ca6d93fe301b0
anrabana <anrabana@gmail.com>
2022-01-17 18:47:26 +0300

    update

--- src/components/keys/Keys.js
+++ src/components/keys/Keys.js
@@ -1,12 +1,14 @@
 import React,{Component} from "react";
 import dataKey from '../../data/keys.json'
+import * as Tone from 'tone'
 import './style.css'
 export default class Keys extends Component{
     constructor(){
         super()
         this.state={
             countKeys:64,
-            data:dataKey
+            data:dataKey,
+            octave:'3'
         }
         this.pressKey=this.pressKey.bind(this)
         this.pressKeyUp=this.pressKeyUp.bind(this)
@@ -14,38 +16,61 @@ export default class Keys extends Component{
     }
 
     pressKeyUp = (oEvent)=>{
-        oEvent.currentTarget.style.opacity='100%'
+        var color = oEvent.target.dataset.color
+        
+        oEvent.currentTarget.style.backgroundColor = color;
     }
     pressKeyDown = (oEvent)=>{
-        oEvent.currentTarget.style.opacity='50%'
+        oEvent.currentTarget.style.backgroundColor="#0437F2"
+        const synth = new Tone.Synth().toDestination();
+        synth.triggerAttackRelease(oEvent.currentTarget.dataset.note, "8n");
     }
     pressKey=(oEvent)=>{
-
+    
     }
     
     render(){
         const keys = []; 
+
+        document.addEventListener('keydown', (oEvent)=>{
+            if(oEvent.key=='x'){
+                
+                // if(this.state.octave<5)
+                this.setState({octave:4})
+                console.log(this.state.octave);
+            }
+            if(oEvent.key=='z'){
+                // if(this.state.octave>2)
+                this.setState({octave:2})
+                
+            }
+        })
+
 
         return(
             <>
             <div className="KeyBoard">
                 {
                     this.state.data.map((el)=>{
-                        return(
-                            
-                            
-                        <div style={{height:'20px',display:'flex',alignItems:'center'}}>
+                        
+                        if(el.note.split('').pop() ==this.state.octave){
+
+                            return(
+                                <div style={{height:'20px',display:'flex',alignItems:'center'}}>
                              <div className="keyNote" 
                              onMouseDown={this.pressKeyDown}
                               onMouseUp={this.pressKeyUp} 
                               onClick={this.pressKey} 
                               onMouseLeave={this.pressKeyUp}
+                              data-color = {el.colorkey}
+                              data-note = {el.note}
                               style={{height:'20px',width:'80px', backgroundColor:el.colorkey,border:'1px solid black'}}/>
                             <h2 className="keyNoteText" style={{display:el.visible}} > {el.note}</h2>
                         </div>
                             
-                 
-                )
+                            
+                            )
+                        }
                     })
                 }
             </div>
